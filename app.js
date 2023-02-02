1ad0194fda11ce3d1d5201d41a8366e95b1809d0
iM4c <luizfelipelb2018@gmail.com>
2023-02-02 13:57:39 -0300

    portifolio0.2.1

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    new TypeIt(".animated", {
+        speed: 200,
+        strings: "Full Stack & Salesforce Jr.",
+        loop: true
+    }).go()
+})
